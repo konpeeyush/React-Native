641fe583d44c5912e5a49f276b6f4c3d19a117f2
Peeyush Kumar Singh <91616196+konpeeyush@users.noreply.github.com>
2023-12-21 17:13:30 +0530

    Updating till Home Page

---
+++ Capestone/components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { TouchableOpacity, Image } from 'react-native'
+
+import styles from './screenheader.style'
+
+const ScreenHeaderBtn = ({ iconUrl, dimensions, handlePress }) => {
+  return (
+    <View>
+      <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+        <Image
+          source={iconUrl}
+          resizeMode='cover'
+          style={styles.btnImg(dimensions)}
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default ScreenHeaderBtn

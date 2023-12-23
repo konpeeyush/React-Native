7c6d5fd1c4da57e56677109c6565b69212edfc7f
Peeyush Kumar Singh <91616196+konpeeyush@users.noreply.github.com>
2023-12-23 13:34:40 +0530

    API expired wadafffff

---
+++ Capestone/hook/useFetch.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'f694b1f168msh75f9ca3800064fap13909cjsna2037e5f55a3',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        params: { ...query },
+    }
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert('There is an error')
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
+}
+
+export default useFetch
+
